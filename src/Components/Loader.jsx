049d1f9eaa60e03436dac86d388bf716ee65381e
Abi <johndoe@example.com>
2023-12-07 11:32:22 +0530

    updating loader

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -1,10 +1,12 @@
 import { Html } from "@react-three/drei";
+import { InfinitySpin } from "react-loader-spinner";
 
 const Loader = () => {
   return (
     <Html>
       <div className="flex justify-center items-center">
-        <div className="w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin" />
+        {/* <div className="w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin" /> */}
+        <InfinitySpin/>
       </div>
     </Html>
   );
